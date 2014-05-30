e966cef87a055742aae0d0c96cef433d70e78bc7
123samson456 <breitung.programming@gmx.de>
2014-05-30 22:12:52 +0200

    Added some new features

--- server/cargodmode.lua
+++ server/cargodmode.lua
@@ -1,31 +1,59 @@
-function invulnerable(args)
+--Setting a variable cargod
+cargod = false
+car = args.player:GetVehicle()
+
+function carenter(args)
+  
+  car = args.player:GetVehicle()
+  
+  if cargod then
+    car:SetInvulnerable(true)
+  else
+    car:SetInvulnerable(false)
+  end
+    
+end
+
+
+function toggle(args)
 
   if args.player:InVehicle() then
 
-    if args.text == "/caron" then
-
-      --Sets Vehicle Invulnerable
-      local on = args.player:GetVehicle()
-      on:SetInvulnerable(true)
-
-      --Message Output
-      args.player:SendChatMessage("Car Godmode is on!",Color(0,255,0))
-
-    end
-
-    if args.text == "/caroff" then
-
-     --Resets Vehicle Attributes (Invulnerable())
-     local on = args.player:GetVehicle()
-     on:SetInvulnerable(false)
-
-     --Message Output
-     args.player:SendChatMessage("Car Godmode is off!",Color(0,255,0))
-
+    if not cargod then
+      if args.text == "/cargod" then
+        --Sets Vehicle Invulnerable
+        local on = args.player:GetVehicle()
+        on:SetInvulnerable(true)
+        --Message Output
+        args.player:SendChatMessage("Car Godmode is on!",Color(0,255,0))
+        --Setting cargod true
+        cargod = true  
+      end
+    else
+      if args.text == "/cargod" then
+        --Resets Vehicle Attributes (Invulnerable())
+        local on = args.player:GetVehicle()
+        on:SetInvulnerable(false)
+        --Message Output
+        args.player:SendChatMessage("Car Godmode is off!",Color(0,255,0))
+        --Setting cargod false
+        cargod = false
+      end
     end
 
   end
 
 end
 
-Events:Subscribe("PlayerChat", invulnerable)
+
+function carexit(args)
+  
+  car:SetInvulnerable(false)
+  
+end
+    
+    
+
+Events:Subscribe("PlayerChat", toggle)
+Events:Subscribe("PlayerEnterVehicle", carenter)
+Events:Subscribe("PlayerExitVehicle", carexit)
