9d6f8badd12fa671fda4f293f7b01665c0f353bc
123samson456 <breitung.programming@gmx.de>
2014-07-26 19:39:14 +0200

    Update cargodmode.lua

--- server/cargodmode.lua
+++ server/cargodmode.lua
@@ -1,71 +1,77 @@
-god = {}
-godtime = Timer()
-
-function join(args)
-  args.player:SetValue("cargod",false)
-end
-
-
-function carenter(args)
-  car = args.player:GetVehicle()
-  if args.player:GetValue("cargod") == false then
-    car:SetInvulnerable(false)
-  end
-  if args.player:GetValue("cargod") == true then
-    car:SetInvulnerable(true)
-  end
-end
-
-
-function toggle(args)
-  cg = args.player:GetValue("cargod")
-  if args.text == "/cargod" then
-    if args.player:GetWorld() == DefaultWorld then
-      if cg == true then
-        args.player:SetValue("cargod",false)
-        if args.player:InVehicle() then
-          args.player:GetVehicle():SetInvulnerable(false)
+function GetPlayerBySteamId( steamid_string )
+    for player in Server:GetPlayers() do
+        if player:GetSteamId().string == steamid_string then
+            return player
         end
-        local find = table.find(god,args.player)
-        table.remove(god,find)
-        args.player:SendChatMessage("Car Godmode has been disabled.",Color(0,255,0))
-      end
-      if cg == false then
-        args.player:SetValue("cargod",true)
-        if args.player:InVehicle() then
-          args.player:GetVehicle():SetInvulnerable(true)
-        end
-        table.insert(god,args.player)
-        args.player:SendChatMessage("Car Godmode has been enabled.",Color(0,255,0))
-      end
-    else
-      args.player:SendChatMessage("You have to be in the main world",Color(255,0,0))
     end
-  end
 end
 
+class 'CarGod'
 
-function carexit(args)
-  car:SetInvulnerable(false)
+function CarGod:__init()
+    self.godTable = {}
+    self.godTimer = Timer()
+    self.checkInterval = 0.5
+    Events:Subscribe( "PlayerChat", self, self.PlayerChat )
+    Events:Subscribe( "PlayerEnterVehicle", self, self.EnterVehicle )
+    Events:Subscribe( "PlayerExitVehicle", self, self.ExitVehicle )
+    Events:Subscribe( "PostTick", self, self.PostTick )
 end
 
-function checkhp()
-  if godtime:GetSeconds() > 0.5 then
-    godtime:Restart()
-    for index,player in pairs(god) do
-      if player:InVehicle() then
-        local veh = player:GetVehicle()
-        if veh:GetHealth() < .5 then
-          veh:SetHealth(1)
-          player:SendChatMessage("Your vehicle has been repaired to avoid a explosion",Color(0,255,0))
+function CarGod:PlayerChat( args )
+    if args.text:lower( ) == "/cargod" then
+        local playerValue = args.player:GetValue( "CarGod" )
+        if playerValue == true then
+            args.player:SetValue( "CarGod", false )
+            args.player:SendChatMessage( "[CarGod] CarGod disabled.", Color.Gold )
+            if IsValid( args.player:GetVehicle() ) then
+                local vehicle = self.godTable[args.player:GetSteamId().string]
+                vehicle:SetInvulnerable( false )
+                vehicle = nil
+            end
+        else
+            args.player:SetValue( "CarGod", true )
+            args.player:SendChatMessage( "[CarGod] CarGod enabled.", Color.Gold )
+            self:EnterVehicle( args )
         end
-      end
     end
-  end
+    if args.text == "/sky" then
+        args.player:SetPosition( args.player:GetPosition() + Vector3( 0, 500, 0 ) )
+    end
 end
 
-Events:Subscribe("PlayerJoin", join)
-Events:Subscribe("PlayerChat", toggle)
-Events:Subscribe("PlayerEnterVehicle", carenter)
-Events:Subscribe("PlayerExitVehicle", carexit)
-Events:Subscribe("PostTick", checkhp)
+function CarGod:EnterVehicle( args )
+    local playerValue = args.player:GetValue( "CarGod" )
+    if playerValue then
+        local vehicle = args.player:GetVehicle()
+        if args.player:InVehicle() then
+            vehicle:SetInvulnerable( true )
+            self.godTable = {
+                [args.player:GetSteamId().string] = vehicle
+            }
+            print( tostring(self.godTable[args.player:GetSteamId().string]) )
+        end
+    end
+end
+
+function CarGod:ExitVehicle( args )
+    local vehicle = self.godTable[args.player:GetSteamId().string]
+    if IsValid( vehicle ) then
+        vehicle:SetInvulnerable( false )
+        vehicle = nil
+    end
+end
+
+function CarGod:PostTick( )
+    if self.godTimer:GetSeconds() >= self.checkInterval then
+        for steamid, vehicle in pairs( self.godTable ) do
+            if IsValid( vehicle ) then
+                if GetPlayerBySteamId( steamid ):GetValue( "CarGod" ) == true then
+                    vehicle:SetHealth( 1 )
+                end
+            end
+        end
+    end
+end
+
+CarGod = CarGod()
